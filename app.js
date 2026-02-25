856965fdd69d6b213b50be0414fae4ce23b7f911
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-02-24 21:42:54 -0800

    Updating confirm.ejs with proper fields

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.get('/', (req, res) => {
 app.get('/admin', (req, res) => {
   res.send(form_data);
 });
+
+
 app.post('/submit',(req, res) =>{
   const submission = {
     name: req.body['order-name'],
@@ -46,13 +48,15 @@ app.post('/submit',(req, res) =>{
     comment: req.body['comments'],
     timestamp: new Date()
   };
+
   form_data.push(submission)
-  res.sendFile(`${import.meta.dirname}/views/confirm.ejs`);
+
+  res.render(`confirm`);
 });
 
 // Thank you route
 app.get('/thank-you', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/confirm.ejs`);
+    res.render('confirm');
 });
 
 
