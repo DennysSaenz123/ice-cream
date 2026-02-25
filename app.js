0716b4625fe3e53da090b7aa9738198354929fbb
Jaron <jayisaac6706@gmail.com>
2026-02-24 21:16:04 -0800

    In-memory data saving for form

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.set('view engine', 'ejs');
 
 const PORT = 3000;
 
-
+// in-memory array
+const form_data = [];
 
  app.use(express.static('public'));
  
@@ -27,8 +28,20 @@ const PORT = 3000;
 // res: allows us to send back a response to the client
 app.get('/', (req, res) => {
 
-  res.sendFile(`${import.meta.dirname}/views/home.html`);
+  res.sendFile(`${import.meta.dirname}/views/index.html`);
 
+});
+app.post('/submit',(req,res) =>{
+  const submission = {
+    name: req.body.name,
+    email: req.body.email,
+    cone_type: req.body.cone_type,
+    toppings: req.body.toppings ? req.body.toppings : "none",
+    comment: req.body.comment,
+    timestamp: new Date()
+  };
+  form_data.push(submission)
+  console.log(form_data);
 });
 
 
